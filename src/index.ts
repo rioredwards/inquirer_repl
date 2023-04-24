814144785118946d78fc21652262c926099eed2a
Rio Edwards <rioredwards@gmail.com>
2023-04-23 19:13:43 -0700

    Displaying table with inquirer babayyyyyyyyyyy

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,41 @@
-import inquirer, { QuestionCollection } from "inquirer";
+/**
+ * List prompt example
+ */
 
-async function main() {
-  let value: string;
-  const answers = await inquirer.prompt({
-    name: "value",
-    type: "input",
-    message: "What is your name?",
+import inquirer from "inquirer";
+import fuzzy from "fuzzy";
+import inquirerPrompt from "inquirer-autocomplete-prompt";
+import { drawTable } from "./table.js";
+import { hotkeys } from "./hotkeys.js";
+
+inquirer.registerPrompt("autocomplete", inquirerPrompt);
+
+function searchHotkeys(answers: any, input = "") {
+  return new Promise((resolve) => {
+    const results = fuzzy.filter(input, hotkeys, {
+      extract: (el) => (el[0] + el[2]).toString(),
+    });
+
+    const matches = results.map((el) => hotkeys[el.index]);
+    const table = drawTable(matches);
+
+    resolve(table);
   });
-  if (answers.value) {
-    value = answers.value;
-    console.log(`Hello ${value}`);
-  } else {
-    console.error("No value provided");
-  }
 }
 
-main()
-  .then(() => process.exit(0))
-  .catch((err) => {
-    console.error(err);
-    process.exit(1);
+inquirer
+  .prompt([
+    {
+      type: "autocomplete",
+      name: "hotkeys",
+      suggestOnly: false,
+      message: "Search for a hotkey",
+      searchText: "Searching...",
+      emptyText: "Nothing found!",
+      source: searchHotkeys,
+      pageSize: 24,
+    },
+  ])
+  .then((answers) => {
+    console.log(answers);
   });
