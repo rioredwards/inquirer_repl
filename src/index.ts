82725f824ea3838246cf41d7a9dfde5a22d3e9a5
Rio Edwards <rioredwards@gmail.com>
2023-04-23 22:11:35 -0700

    Refactor to be more readable + type-safe

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,48 @@
-/**
- * List prompt example
- */
-
-import inquirer from "inquirer";
+import inquirer, { Answers } from "inquirer";
 import fuzzy from "fuzzy";
 import inquirerPrompt from "inquirer-autocomplete-prompt";
 import { drawTable } from "./table.js";
 import { hotkeys } from "./hotkeys.js";
 
-inquirer.registerPrompt("autocomplete", inquirerPrompt);
+async function main() {
+  inquirer.registerPrompt("autocomplete", inquirerPrompt);
 
-function searchHotkeys(answers: any, input = "") {
-  return new Promise((resolve) => {
-    const results = fuzzy.filter(input, hotkeys, {
-      extract: (el) => (el[0] + el[2]).toString(),
+  function searchHotkeys(answers: Answers, input = "") {
+    return new Promise((resolve) => {
+      const results = fuzzy.filter(input, hotkeys, {
+        extract: (hotkey) => [...hotkey].toString(),
+      });
+
+      if (results.length === 0) resolve(["No results found"]);
+      const matches = results.map((hotkey) => hotkeys[hotkey.index]);
+      const table = drawTable(matches);
+
+      resolve(table);
     });
+  }
 
-    if (results.length === 0) resolve(["No results found"]);
-    const matches = results.map((el) => hotkeys[el.index]);
-    const table = drawTable(matches);
-
-    resolve(table);
-  });
+  try {
+    const answers: Answers = await inquirer.prompt([
+      {
+        type: "autocomplete",
+        name: "hotkeys",
+        suggestOnly: false,
+        message: "Search for a hotkey",
+        searchText: "Searching...",
+        emptyText: "Nothing found!",
+        source: searchHotkeys,
+        pageSize: 24,
+      },
+    ]);
+    console.log(answers);
+  } catch (err) {
+    console.error(err);
+  }
 }
 
-inquirer
-  .prompt([
-    {
-      type: "autocomplete",
-      name: "hotkeys",
-      suggestOnly: false,
-      message: "Search for a hotkey",
-      searchText: "Searching...",
-      emptyText: "Nothing found!",
-      source: searchHotkeys,
-      pageSize: 24,
-    },
-  ])
-  .then((answers) => {
-    console.log(answers);
+main()
+  .then(() => process.exit(0))
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
   });
